525346d462a0288bde6615b63db6b7d2deaa2304
~ Leo <alone971681@gmail.com>
2026-06-21 13:12:13 +0530

    Delete web.py

--- web.py
+++
@@ -1,7 +0,0 @@
-from app import app
-
-# Export the app for Gunicorn to use
-application = app  # Alias for compatibility
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
